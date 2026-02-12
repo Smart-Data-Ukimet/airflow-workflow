5db1ea2be3117012bbfc307cb45e06ade5713798
armat_kabulsikov <armat.kabulsikov@nitec.kz>
2026-02-12 16:45:37 +0500

    Fix model

--- dbt_demo/models/demo_sales_data_test.sql
+++ dbt_demo/models/demo_sales_data_test.sql
@@ -4,32 +4,32 @@
     )
 }}
 
-SELECT
-    order_number
-    , quantity_ordered
-    , price_each
-    , order_linenumber
-    , sales
-    , order_date
-    , `status`
-    , qtr_id
-    , month_id
-    , year_id
-    , product_line
-    , msrp
-    , product_code
-    , customer_name
-    , phone
-    , address_line_1
-    , address_line_2
-    , city
-    , `state`
-    , postal_code
-    , country
-    , territory
-    , contact_lastname
-    , contact_firstname
-    , deal_size
-    , NOW() AS load_date
+select
+  toInt64(order_number)          as order_number,
+  toInt64(quantity_ordered)      as quantity_ordered,
+  toFloat64(price_each)          as price_each,
+  toInt64(order_linenumber)      as order_linenumber,
+  toFloat64(sales)               as sales,
+  toDateTime(order_date)         as order_date,
+  status                         as status,
+  toInt64(qtr_id)                as qtr_id,
+  toInt64(month_id)              as month_id,
+  toInt64(year_id)               as year_id,
+  product_line                   as product_line,
+  toInt64(msrp)                  as msrp,
+  product_code                   as product_code,
+  customer_name                  as customer_name,
+  phone                          as phone,
+  address_line_1                 as address_line_1,
+  address_line_2                 as address_line_2,
+  city                           as city,
+  state                          as state,
+  postal_code                    as postal_code,
+  country                        as country,
+  territory                      as territory,
+  contact_lastname               as contact_lastname,
+  contact_firstname              as contact_firstname,
+  deal_size                      as deal_size,
+  toDateTime(load_date)          as load_date
 FROM
     {{ source('demo', 'sales_data_test') }}
